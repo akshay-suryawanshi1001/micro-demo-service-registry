47e8996bf38444eb4297021d9898536aec29521d
akshay-suryawanshi1001 <akshay.suryawanshi1001@gmail.com>
2022-09-10 21:58:29 +0530

    upload project

---
+++ src/test/java/com/example/microdemoserviceregistry/MicroDemoServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microdemoserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroDemoServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
